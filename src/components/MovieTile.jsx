e375a376a60676f35ebbbfd54d55f44165661a9d
adhe223 <adhe223@g.uky.edu>
2017-01-26 19:27:14 -0700

    Initialize application

---
+++ src/components/MovieTile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MovieTile extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default MovieTile;
